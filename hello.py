8191dc7b4169dbae2e89365f8e08c9a129175785
Jusen <1537017271@qq.com>
2019-04-25 19:05:54 +0800

    fix bugs

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route('/')
 def hello():
 	logging.debug("saying hello heroku!")
-	return 'Hello Heroku!, It's so cool!
+	return 'Hello Heroku!, It\'s so cool!'
